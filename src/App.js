43631d60a349c5e7c711eace116919660e5fd0ce
Kari Lavikka <kari.lavikka@helsinki.fi>
2024-05-16 14:14:03 +0300

    add GenomeSpy component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import GenomeSpy from "@genome-spy/react-component";
+
+/**
+ * @type {import("@genome-spy/core/spec/root.js").RootSpec}
+ */
+const spec = {
+  data: {
+    name: "dynamicData",
+  },
+  hconcat: [
+    {
+      height: 250,
+      width: 500,
+      mark: "rect",
+      encoding: {
+        x: {
+          field: "id",
+          type: "nominal",
+          scale: {
+            padding: 0.1,
+          },
+        },
+        y: {
+          field: "y",
+          type: "quantitative",
+          scale: { domain: [0, 1] },
+        },
+      },
+    },
+  ],
+};
+
+const data = [
+  { id: 1, x: 1, y: 0.2 },
+  { id: 2, x: 2, y: 0.5 },
+  { id: 3, x: 3, y: 1 },
+];
+
+/** @type {(api: import("@genome-spy/core/types/embedApi.js").EmbedResult) => void} */
+const updateData = (api) => {
+  api.updateNamedData("dynamicData", data);
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Using GenomeSpy in a React App</h1>
       </header>
+      <GenomeSpy spec={spec} onEmbed={updateData} />
     </div>
   );
 }
